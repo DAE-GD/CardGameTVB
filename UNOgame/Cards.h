c2565fa0f81f8f42af4c3ec4f88c9114ad9a204f
thibau verberckmoes <thibauverberckmoes@gmail.com>
2019-09-08 12:56:38 +0200

    Second Card Update

--- UNOgame/Cards.h
+++ UNOgame/Cards.h
@@ -2,11 +2,47 @@
 class Cards
 {
 public:
-
+	void CardReset()
+	{
+		//Normal cards
+		m_ArryCards[0][0] = 0;
+		m_ArryCards[0][1] = 0;
+	}
 private:
 	/*
-		Normal Cards = 0
+		Normal cards == 0
+		'0' Cards ==1 //Are seen as special cards because they do not have 2 time Each color only ones//
+		Color change cards normal == 2
+		Color change cards +4 == 3
+		Block cards = 4
+		Reverse cards == 5
+		+ 2 cards == 6
+	
+
+		Blue == 0
+		Yellow == 1
+		RED == 2
+		GREEN == 3
+		Black == 4
+
+		Digit == 0 -- 9
+
+	+----+---+-------+-------+--------+
+	| ID | 0 | Digit | color | Amount |
+	+----+---+-------+-------+--------+
+	| ID | 1 | Digit | color | Amount |
+	+----+---+-------+-------+--------+
+	| ID | 2 | Digit | color | Amount |
+	+----+---+-------+-------+--------+
+	| ID | 3 | Digit | color | Amount |
+	+----+---+-------+-------+--------+
+	| ID | 4 | Digit | color | Amount |
+	+----+---+-------+-------+--------+
+	| ID | 5 | Digit | color | Amount |
+	+----+---+-------+-------+--------+
 
 	*/
+
+	int m_ArryCards[7][4]{};
 };
 
